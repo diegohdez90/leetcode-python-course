7b8fc9a84b8100262b084b053ef91494349f30fd
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-24 14:11:40 -0600

    feat(big-o): drop constant

--- big-o/main.py
+++ big-o/main.py
@@ -2,7 +2,10 @@
 def print_items(n):
     for i in range(n):
         print(i)
-
+    
+    # it still O(n) not O(2n)
+    # for j in range(n):
+    #     print(j)
 
 if __name__ == '__main__':
     print_items(10)

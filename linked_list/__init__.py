2b47064844c028d2d269505d43b8ecdbf82c8099
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-24 14:56:38 -0600

    feat(linked_list): add constructor

--- linked_list/__init__.py
+++ linked_list/__init__.py
@@ -1,3 +1,10 @@
+from .node import Node
+
 class LinkedList:
     head = None
     tail = None
+    
+    def __init__(self, value):
+        node = Node(value)
+        self.head = node
+        self.tail = node

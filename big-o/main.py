b6afe4a6e37d92f3b0044a2a0c77c4a07cb4ba82
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-24 14:13:33 -0600

    feat(big-o): add O(n^2)

--- big-o/main.py
+++ big-o/main.py
@@ -7,6 +7,12 @@ def print_items(n):
     # for j in range(n):
     #     print(j)
 
+# O(n^2)
+def print_items_2(n):
+    for i in range(n):
+        for j in range(n):
+            print(i,j)
+
 if __name__ == '__main__':
-    print_items(10)
+    print_items_2(10)
 

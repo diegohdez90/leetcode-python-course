bf438940aeeb4b22a8bfa5ad1723b81be1c5a10b
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-27 13:19:07 -0600

    test(linked_list): test length attribute

--- linked_list/test/main.py
+++ linked_list/test/main.py
@@ -5,6 +5,7 @@ def test_linked_list_init_value():
     ll = LinkedList(6)
     assert ll.head.value == 6, "head should be 6"
     assert ll.tail.value == 6, "tail should be 6"
+    assert ll.length == 1, 'length must be 1'
     
 if __name__ == '__main__':
     test_linked_list_init_value()

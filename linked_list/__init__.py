187ced986a5186940d12c5006bdb3f537f2baaf2
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-31 14:48:01 -0600

    feat(linked_list): if is empty add new node as head and tail

--- linked_list/__init__.py
+++ linked_list/__init__.py
@@ -12,9 +12,13 @@ class LinkedList:
     
     def append(self, value):
         node = Node(value)
-        tmp = self.tail
-        tmp.pointer = node
-        self.tail = node
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            tmp = self.tail
+            tmp.pointer = node
+            self.tail = node
         self.length += 1
         
     def print_list(self):

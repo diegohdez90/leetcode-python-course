d167e7a58c076a44b7ba7615a52dd69718904eba
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-31 14:41:29 -0600

    test(app): linked list append method

--- test/test_linked_list.py
+++ test/test_linked_list.py
@@ -8,6 +8,14 @@ class test_linked_list(unittest.TestCase):
         self.assertEqual(ll.head.value, 6)
         self.assertEqual(ll.tail.value, 6)
         self.assertEqual(ll.length, 1)
+        
+    def test_append(self):
+        ll = LinkedList(9)
+        ll.append(16)
+        self.assertEqual(ll.head.value, 9)
+        self.assertEqual(ll.head.pointer.value, 16)
+        self.assertEqual(ll.tail.value, 16)
+        self.assertEqual(ll.length, 2)
 
 if __name__ == '__main__':
     unittest.main()

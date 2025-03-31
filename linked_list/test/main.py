bde25df51d644e57305d51de21ddaf7d6d3d1723
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-31 14:41:02 -0600

    test(linked_list): append method

--- linked_list/test/main.py
+++ linked_list/test/main.py
@@ -1,4 +1,4 @@
-from .. import LinkedList
+from linked_list import LinkedList
 from log import log
 
 def test_linked_list_init_value():
@@ -6,7 +6,17 @@ def test_linked_list_init_value():
     assert ll.head.value == 6, "head should be 6"
     assert ll.tail.value == 6, "tail should be 6"
     assert ll.length == 1, 'length must be 1'
+
+def test_linked_list_append():
+    ll = LinkedList(6)
+    ll.append(2)
+    assert ll.head.value == 6, "head should be 6"
+    assert ll.tail.value == 2, "tail should be 2"
+    assert ll.head.pointer.value == 2, 'head value 6 pointer to 2'
+    assert ll.length == 2, 'length must be 1'
+    
     
 if __name__ == '__main__':
     test_linked_list_init_value()
+    test_linked_list_append()
     log.info('Test passed')

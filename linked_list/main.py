228d9f6576f84aa0e928ddd1dbfe100562873410
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-31 14:40:19 -0600

    feat(linked_list): add append method

--- linked_list/main.py
+++ linked_list/main.py
@@ -1,7 +1,14 @@
-from . import LinkedList
+import sys
+import os
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from linked_list import LinkedList
 
 if __name__ == '__main__':
     ll = LinkedList(7)
     print(ll.head.value)
+    print(ll.tail.value)
+    ll.append(10)
     print(ll.tail.value)
     ll.print_list()
